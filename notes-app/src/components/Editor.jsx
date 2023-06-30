3b07373f0b9ab50dab71d83d72ded4477171fba8
giremunga <134525119+giremunga@users.noreply.github.com>
2023-06-30 23:19:54 +0300

    first commit

---
+++ notes-app/src/components/Editor.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Editor = ({ currentNote, updateNote }) => {
+    const [selectedTab, setSelectedTab] = React.useState("write")
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })  
+
+    return (
+        <section className="pane editor">
+            <ReactMde
+                value={currentNote.body}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+        </section>
+    )
+}
+
+    
+      
+    
+
+
+
+export default Editor
